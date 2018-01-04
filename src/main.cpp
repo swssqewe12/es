5b7bd11ea2e1d2d52484a4bf2238b781e28be7d4
swssqewe12 <david@callanan.ie>
2018-01-04 12:31:24 +0000

    Added in file open error check

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,30 @@
 #include <conio.h>
+#include <stdlib.h>
 #include "hstr.h"
 #include "Token.h"
 #include "Lexer.h"
 
+FILE* readFile(const char* src)
+{
+    FILE* file = fopen(src, "r");
+
+    if (file == NULL)
+    {
+        printf("Failed to open ");
+        printf(src);
+        printf(": ");
+        perror("");
+        exit(EXIT_FAILURE);
+    }
+
+    return file;
+}
+
 int main()
 {
-    FILE* file = fopen("res\\script.txt", "r");
+    printf("Opening file...\n");
+    FILE* file = readFile("res/script.es");
+
     Lexer lexer(file);
     List<Token>* tokens = lexer.make_tokens();
     fclose(file);
